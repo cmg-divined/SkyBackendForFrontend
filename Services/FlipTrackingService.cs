b129a28f0edb94871c7811ba2a9fce22927ab74f
Äkwav <xekwavx@gmail.com>
2021-12-31 21:16:36 +0100

    add capability to get sells for finder

--- Services/FlipTrackingService.cs
+++ Services/FlipTrackingService.cs
@@ -13,6 +13,7 @@ namespace Coflnet.Sky.Commands
     public partial class FlipTrackingService
     {
         public TrackerApi flipTracking;
+        private AnalyseApi flipAnalyse;
 
         public static FlipTrackingService Instance = new FlipTrackingService();
 
@@ -36,6 +37,7 @@ namespace Coflnet.Sky.Commands
             })
                     .SetValueSerializer(hypixel.SerializerFactory.GetSerializer<FlipTracker.Client.Model.FlipEvent>()).Build();
             flipTracking = new TrackerApi("http://" + SimplerConfig.Config.Instance["FLIPTRACKER_HOST"]);
+            flipAnalyse = new AnalyseApi("http://" + SimplerConfig.Config.Instance["FLIPTRACKER_HOST"]);
         }
 
 
@@ -100,6 +102,58 @@ namespace Coflnet.Sky.Commands
             });
         }
 
+        public async Task<List<FlipDetails>> GetFlipsForFinder(LowPricedAuction.FinderType type, DateTime start, DateTime end)
+        {
+            if (start > end)
+            {
+                var tmp = end;
+                end = start;
+                start = tmp;
+            }
+            if (start < end - TimeSpan.FromDays(1))
+                throw new CoflnetException("span_to_large", "Querying for more than a day is not supported");
+
+            var idTask = flipAnalyse.AnalyseFinderFinderTypeIdsGetAsync((Coflnet.Sky.FlipTracker.Client.Model.FinderType)type, start, end);
+            using (var context = new HypixelContext())
+            {
+                var ids = await idTask;
+                var buyList = await context.Auctions.Where(a => ids.Contains(a.UId))
+                    .Include(a => a.NBTLookup)
+                    .ToListAsync();
+                
+                var uidKey = NBT.Instance.GetKeyId("uid");
+                var buyLookup = buyList
+                    .Where(a => a.NBTLookup.Where(l => l.KeyId == uidKey).Any())
+                    .GroupBy(a =>
+                    {
+                        return a.NBTLookup.Where(l => l.KeyId == uidKey).FirstOrDefault().Value;
+                    }).ToDictionary(b=>b.Key);
+                var buyUidLookup = buyLookup.Select(a => a.Key).ToHashSet();
+                var sellIds = await context.NBTLookups.Where(b => b.KeyId == uidKey && buyUidLookup.Contains(b.Value)).Select(n => n.AuctionId).ToListAsync();
+                var sells = await context.Auctions.Where(b =>sellIds.Contains(b.Id) && b.End > start).Select(s => new {s.End,s.HighestBidAmount,s.NBTLookup,s.Uuid}).ToListAsync();
+
+                return sells.Select(s=>{
+                    var uid = s.NBTLookup.Where(b => b.KeyId == uidKey).FirstOrDefault().Value;
+                    var buy = buyLookup.GetValueOrDefault(uid)?.FirstOrDefault();
+                    return new FlipDetails()
+                    {
+                        BuyTime = buy.End,
+                        Finder = type,
+                        ItemName = buy.ItemName,
+                        ItemTag = buy.Tag,
+                        OriginAuction = buy.Uuid,
+                        PricePaid = buy.HighestBidAmount,
+                        SellTime = s.End,
+                        SoldAuction = s.Uuid,
+                        SoldFor = s.HighestBidAmount,
+                        Tier = buy.Tier.ToString(),
+                        uId = uid
+                    };
+                }).ToList();
+            }
+
+        }
+
         public async Task<FlipSumary> GetPlayerFlips(string uuid, TimeSpan timeSpan)
         {
             using (var context = new HypixelContext())
@@ -116,11 +170,10 @@ namespace Coflnet.Sky.Commands
                     .Where(a => a.NBTLookup.Where(l => l.KeyId == uidKey).Any())
                     .GroupBy(a =>
                     {
-                        //Console.WriteLine($"{a.ItemName} {a.NBTLookup.Where(l => l.KeyId == uidKey).FirstOrDefault().Value} {a.Uuid} {a.End}");
                         return a.NBTLookup.Where(l => l.KeyId == uidKey).FirstOrDefault().Value;
                     }).ToList();
                 var SalesUidLookup = sells.Select(a => a.Key).ToHashSet();
-                var sales = await context.NBTLookups.Where(b => b.KeyId == uidKey && SalesUidLookup.Contains(b.Value)).Select(n=>n.AuctionId).ToListAsync();
+                var sales = await context.NBTLookups.Where(b => b.KeyId == uidKey && SalesUidLookup.Contains(b.Value)).Select(n => n.AuctionId).ToListAsync();
                 var playerBids = await context.Bids.Where(b => b.BidderId == playerId && sales.Contains(b.Auction.Id) && b.Timestamp > startTime.Subtract(TimeSpan.FromDays(14)))
                     //.Where(b => b.Auction.NBTLookup.Where(b => b.KeyId == uidKey && SalesUidLookup.Contains(b.Value)).Any())
                     // filtering
@@ -145,12 +198,12 @@ namespace Coflnet.Sky.Commands
                     //.ThenInclude (b => b.Auction)
                     .ToListAsync();
 
-                var flipStats = (await flipTracking.TrackerBatchFlipsPostAsync(playerBids.Select(b=>AuctionService.Instance.GetId(b.Key)).ToList()))
-                        .GroupBy(t=>t.AuctionId)
-                        .ToDictionary(t=>t.Key,v=>v.AsEnumerable());
+                var flipStats = (await flipTracking.TrackerBatchFlipsPostAsync(playerBids.Select(b => AuctionService.Instance.GetId(b.Key)).ToList()))
+                        ?.GroupBy(t => t.AuctionId)
+                        ?.ToDictionary(t => t.Key, v => v.AsEnumerable());
                 var flips = playerBids.Where(a => SalesUidLookup.Contains(a.itemUid)).Select(b =>
                 {
-                    FlipTracker.Client.Model.Flip first = flipStats.GetValueOrDefault(AuctionService.Instance.GetId(b.Key))?.OrderBy(b=>b.Timestamp).FirstOrDefault();
+                    FlipTracker.Client.Model.Flip first = flipStats?.GetValueOrDefault(AuctionService.Instance.GetId(b.Key))?.OrderBy(b => b.Timestamp).FirstOrDefault();
 
                     var sell = sells.Where(s => s.Key == b.itemUid)?
                             .FirstOrDefault()
@@ -174,9 +227,11 @@ namespace Coflnet.Sky.Commands
                     };
                 }).ToArray();
 
-                return new FlipSumary() { 
-                    Flips = flips, 
-                    TotalProfit = flips.Sum(r=>r.SoldFor-r.PricePaid) };
+                return new FlipSumary()
+                {
+                    Flips = flips,
+                    TotalProfit = flips.Sum(r => r.SoldFor - r.PricePaid)
+                };
             }
         }
     }
