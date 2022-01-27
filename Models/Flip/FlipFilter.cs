84533d520df0ff3b7001bff44734a9e23cdff1a4
Äkwav <xekwavx@gmail.com>
2022-01-27 23:48:38 +0100

    use computed filter

--- Models/Flip/FlipFilter.cs
+++ Models/Flip/FlipFilter.cs
@@ -1,4 +1,5 @@
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Coflnet.Sky.Filter;
@@ -10,16 +11,16 @@ namespace Coflnet.Sky.Commands.Shared
     {
         private static FilterEngine FilterEngine = new FilterEngine();
 
-        private Dictionary<string, string> Filters;
+        private Func<SaveAuction,bool> Filters;
 
         public FlipFilter(Dictionary<string, string> filters)
         {
-            Filters = filters;
+            Filters = FilterEngine.GetMatcher(filters);
         }
 
         public bool IsMatch(FlipInstance flip)
         {
-            return Filters == null || FilterEngine.AddFilters(new SaveAuction[] { flip.Auction }.AsQueryable(), Filters, false).Any();
+            return Filters == null || Filters(flip.Auction);
         }
     }
     
