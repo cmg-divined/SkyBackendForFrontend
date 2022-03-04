f452a04073ed902da8a51c7bbb138a5ca8ad9083
Äkwav <xekwavx@gmail.com>
2022-03-04 17:04:32 +0100

    add price history to service

--- Services/PricesService.cs
+++ Services/PricesService.cs
@@ -51,11 +51,11 @@ namespace Coflnet.Sky.Commands.Shared
 
         public async Task<PriceSumary> GetSumaryCache(string itemTag)
         {
-            var sumary = await CacheService.Instance.GetFromRedis<PriceSumary>("psum"+itemTag);
-            if(sumary == default)
+            var sumary = await CacheService.Instance.GetFromRedis<PriceSumary>("psum" + itemTag);
+            if (sumary == default)
             {
                 sumary = await GetSumary(itemTag, new Dictionary<string, string>());
-                await CacheService.Instance.SaveInRedis("psum"+itemTag,sumary,TimeSpan.FromHours(2));
+                await CacheService.Instance.SaveInRedis("psum" + itemTag, sumary, TimeSpan.FromHours(2));
             }
             return sumary;
         }
@@ -63,6 +63,46 @@ namespace Coflnet.Sky.Commands.Shared
         private static int GetItemId(string itemTag, bool forceget = true)
         {
             return ItemDetails.Instance.GetItemIdForName(itemTag, forceget);
+        }
+
+        public async Task<IEnumerable<AveragePrice>> GetHistory(string itemTag, DateTime start, DateTime end, Dictionary<string, string> filters)
+        {
+            var itemId = GetItemId(itemTag);
+            var select = context.Auctions
+                        .Where(auction => auction.ItemId == itemId)
+                        .Where(auction => auction.End > start && auction.End < end)
+                        .Where(auction => auction.HighestBidAmount > 1);
+            if (filters != null && filters.Count > 0)
+            {
+                filters["ItemId"] = itemId.ToString();
+                select = FilterEngine.AddFilters(select, filters);
+            }
+
+            var groupedSelect = select.GroupBy(item => new { item.End.Date, Hour = 0 });
+            if (end - start < TimeSpan.FromDays(7.001))
+                groupedSelect = select.GroupBy(item => new { item.End.Date, item.End.Hour });
+
+            var dbResult = await groupedSelect
+                .Select(item =>
+                    new
+                    {
+                        End = item.Key,
+                        Avg = (int)item.Average(a => ((int)a.HighestBidAmount) / a.Count),
+                        Max = (int)item.Max(a => ((int)a.HighestBidAmount) / a.Count),
+                        Min = (int)item.Min(a => ((int)a.HighestBidAmount) / a.Count),
+                        Count = item.Sum(a => a.Count)
+                    }).ToListAsync();
+
+            return dbResult
+                .Select(i => new AveragePrice()
+                {
+                    Volume = i.Count,
+                    Avg = i.Avg,
+                    Max = i.Max,
+                    Min = i.Min,
+                    Date = i.End.Date.Add(TimeSpan.FromHours(i.End.Hour)),
+                    ItemId = itemId
+                });
         }
 
         /// <summary>
@@ -108,7 +148,6 @@ namespace Coflnet.Sky.Commands.Shared
                 var cost = count == 1 ? lowestBins.FirstOrDefault().Price : lowestBins.Sum(a => a.Price);
                 return new CurrentPrice() { Buy = cost, Sell = lowestBins.FirstOrDefault()?.Price * 0.99 ?? 0, Available = lowestBins.Count };
             }
-
         }
 
         public double GetBazaarCostForCount(List<dev.BuyOrder> orders, int count)
