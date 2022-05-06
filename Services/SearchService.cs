1c55b116761b992929e9b6007c9f27c086a367e4
ekwav <ekwav@coflnet.com>
2022-05-06 18:12:55 +0200

    include item base rarity in search result

--- Services/SearchService.cs
+++ Services/SearchService.cs
@@ -228,9 +228,10 @@ namespace Coflnet.Sky.Commands.Shared
                 Name = i.Text + (i.Flags.Value.HasFlag(Sky.Items.Client.Model.ItemFlags.BAZAAR) ? " - bazaar"
                         : i.Flags.Value.HasFlag(Sky.Items.Client.Model.ItemFlags.AUCTION) ? "" : " - not on ah"),
                 Tag = i.Tag,
+                Tier = (Coflnet.Sky.Core.Tier)i.Tier,
                 IconUrl = "https://sky.coflnet.com/static/icon/" + i.Tag,
                 HitCount = i.Tag == "CAKE_SOUL" ? 2 : 30 // items higher base hit count
-
+                
             }).ToList();
         }
 
