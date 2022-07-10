53da0a6f07ea5e857f9f105711d70d78cda14a6e
Äkwav <ekwav@coflnet.com>
2022-07-11 00:26:41 +0200

    equals and hashcode for privacy settings

--- Models/PrivacySettings.cs
+++ Models/PrivacySettings.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+
 namespace Coflnet.Sky.Commands.Shared
 {
     public class PrivacySettings
@@ -21,5 +24,42 @@ namespace Coflnet.Sky.Commands.Shared
         /// </summary>
         public string[] CommandPrefixes;
         public bool AutoStart;
+
+        public override bool Equals(object obj)
+        {
+            return obj is PrivacySettings settings &&
+                   ChatRegex == settings.ChatRegex &&
+                   CollectChat == settings.CollectChat &&
+                   CollectInventory == settings.CollectInventory &&
+                   CollectTab == settings.CollectTab &&
+                   CollectScoreboard == settings.CollectScoreboard &&
+                   AllowProxy == settings.AllowProxy &&
+                   CollectInvClick == settings.CollectInvClick &&
+                   CollectChatClicks == settings.CollectChatClicks &&
+                   CollectLobbyChanges == settings.CollectLobbyChanges &&
+                   CollectEntities == settings.CollectEntities &&
+                   ExtendDescriptions == settings.ExtendDescriptions &&
+                   EqualityComparer<string[]>.Default.Equals(CommandPrefixes, settings.CommandPrefixes) &&
+                   AutoStart == settings.AutoStart;
+        }
+
+        public override int GetHashCode()
+        {
+            HashCode hash = new HashCode();
+            hash.Add(ChatRegex);
+            hash.Add(CollectChat);
+            hash.Add(CollectInventory);
+            hash.Add(CollectTab);
+            hash.Add(CollectScoreboard);
+            hash.Add(AllowProxy);
+            hash.Add(CollectInvClick);
+            hash.Add(CollectChatClicks);
+            hash.Add(CollectLobbyChanges);
+            hash.Add(CollectEntities);
+            hash.Add(ExtendDescriptions);
+            hash.Add(CommandPrefixes);
+            hash.Add(AutoStart);
+            return hash.ToHashCode();
+        }
     }
 }
