97dcaa45bceb7a92b03c4c29e505a9a8b62451d8
Äkwav <ekwav@coflnet.com>
2022-08-10 13:23:23 +0200

    add alt name option to class based dictionary

--- Helper/ClassNameDictonary.cs
+++ Helper/ClassNameDictonary.cs
@@ -5,10 +5,12 @@ namespace Coflnet.Sky.Commands.Shared
 {
     public class ClassNameDictonary<T> : Dictionary<string, T>
     {
-        public void Add<TDerived>() where TDerived : T
+        public void Add<TDerived>(string altName = null) where TDerived : T
         {
             var filter = Activator.CreateInstance<TDerived>();
             this.Add(GetCleardName<TDerived>(), filter);
+            if(altName != null)
+                this.Add(altName, filter);
         }
 
         public static string GetCleardName<TDerived>() where TDerived : T
