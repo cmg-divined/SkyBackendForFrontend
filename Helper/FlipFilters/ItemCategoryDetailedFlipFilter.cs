099079137065c929f15f66e67ef49d3910121863
Äkwav <ekwav@coflnet.com>
2023-01-31 19:51:27 +0100

    second level cache item categories

--- Helper/FlipFilters/ItemCategoryDetailedFlipFilter.cs
+++ Helper/FlipFilters/ItemCategoryDetailedFlipFilter.cs
@@ -12,15 +12,32 @@ namespace Coflnet.Sky.Commands.Shared
 {
     public class ItemCategoryDetailedFlipFilter : DetailedFlipFilter
     {
-        public object[] Options => Enum.GetValues<ItemCategory>().Select(t=> (object)t).ToArray();
+        public object[] Options => Enum.GetValues<ItemCategory>().Select(t => (object)t).ToArray();
 
         public FilterType FilterType => FilterType.Equal;
+
+        private static readonly Dictionary<ItemCategory, HashSet<string>> categoryToTags = new Dictionary<ItemCategory, HashSet<string>>();
+        private static DateTime lastUpdate = DateTime.MinValue;
 
         public Expression<Func<FlipInstance, bool>> GetExpression(Dictionary<string, string> filters, string val)
         {
             if (!Enum.TryParse<ItemCategory>(val, true, out ItemCategory itemCategory))
                 throw new CoflnetException("invalid_category", $"the specified category {val} does not exist");
+
+            if (DateTime.Now - lastUpdate > TimeSpan.FromHours(2))
+            {
+                categoryToTags.Clear();
+                lastUpdate = DateTime.Now;
+            }
+            if (categoryToTags.ContainsKey(itemCategory))
+            {
+                var tags = categoryToTags[itemCategory];
+                return flip => tags.Contains(flip.Auction.Tag);
+            }
             var response = DiHandler.ServiceProvider.GetService<Sky.Items.Client.Api.IItemsApi>().ItemsCategoryCategoryItemsGet(itemCategory).ToHashSet();
+            if (response.Count == 0)
+                throw new CoflnetException("no_items", $"No items found for category {itemCategory}");
+            categoryToTags.Add(itemCategory, response);
             return flip => response.Contains(flip.Auction.Tag);
         }
     }
