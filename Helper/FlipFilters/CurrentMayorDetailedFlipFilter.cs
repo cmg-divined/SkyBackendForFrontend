3637d02db2bebcc1c132b4f1db8f4a9b5956b080
Äkwav <ekwav@coflnet.com>
2023-01-31 15:40:42 +0100

    normalize mayor input

--- Helper/FlipFilters/CurrentMayorDetailedFlipFilter.cs
+++ Helper/FlipFilters/CurrentMayorDetailedFlipFilter.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using Coflnet.Sky.Core;
+using System.Linq;
 
 namespace Coflnet.Sky.Commands.Shared;
 public class CurrentMayorDetailedFlipFilter : DetailedFlipFilter
@@ -13,6 +14,10 @@ public class CurrentMayorDetailedFlipFilter : DetailedFlipFilter
 
     public Expression<Func<FlipInstance, bool>> GetExpression(Dictionary<string, string> filters, string val)
     {
+        // normalize the name
+        val = Options.FirstOrDefault(t => t.ToString().ToLower() == val.ToLower()).ToString();
+        if (val == null)
+            throw new CoflnetException("invalid_mayor", "The specified mayor does not exist");
         var current = DiHandler.GetService<Sky.Mayor.Client.Api.IMayorApi>().MayorCurrentGet();
         if (current == null || current.Name == null)
             throw new CoflnetException("no_mayor", "Current mayor could not be retrieved");
