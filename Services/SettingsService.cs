d5b9c86d8f8c0c86f805bd307bab294915a7f5e4
Äkwav <ekwav@coflnet.com>
2023-02-19 19:10:45 +0100

    increase backoff

--- Services/SettingsService.cs
+++ Services/SettingsService.cs
@@ -86,7 +86,7 @@ namespace Coflnet.Sky.Commands.Shared
                 }
                 catch (System.Exception e)
                 {
-                    await Task.Delay(20 * i);
+                    await Task.Delay(300 * (i + 1));
                     if (i > 0)
                         Console.WriteLine($"failed to update settings {e.Message} \n" + JsonConvert.SerializeObject(data));
                     if (i == 2)
