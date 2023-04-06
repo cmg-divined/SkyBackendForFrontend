156e81fa703a58a0b90e11c832b266bfd8da05c1
Äkwav <ekwav@coflnet.com>
2023-04-06 22:18:03 +0200

    return mock auction for non attributes

--- Services/InventoryParser.cs
+++ Services/InventoryParser.cs
@@ -131,7 +131,7 @@ public class InventoryParser
             {
                 yield return new SaveAuction()
                 {
-                    Tag = ExtraAttributes.id.value,
+                    Tag = "UNKOWN",
                     Enchantments = new(),
                     Count = 1,
                     ItemName = item.displayName,
