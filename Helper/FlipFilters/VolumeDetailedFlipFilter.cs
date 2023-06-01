ec2bfbb972322d41122f500665e739a9e51f05f1
Äkwav <ekwav@coflnet.com>
2023-06-01 21:49:58 +0200

    correct min volume

--- Helper/FlipFilters/VolumeDetailedFlipFilter.cs
+++ Helper/FlipFilters/VolumeDetailedFlipFilter.cs
@@ -8,7 +8,7 @@ namespace Coflnet.Sky.Commands.Shared
 
     public class VolumeDetailedFlipFilter : NumberDetailedFlipFilter
     {
-        public override object[] Options => new object[]{1,1000};
+        public override object[] Options => new object[]{0.05,1000};
 
         protected override Expression<Func<FlipInstance, double>> GetSelector()
         {
