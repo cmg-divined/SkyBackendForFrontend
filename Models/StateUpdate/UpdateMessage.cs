6efe71005c83efd8660519e5462c582bfed735c8
Äkwav <ekwav@coflnet.com>
2023-06-04 19:07:01 +0200

    remove item

--- Models/StateUpdate/UpdateMessage.cs
+++ Models/StateUpdate/UpdateMessage.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using MessagePack;
+using Coflnet.Sky.Core;
 
 namespace Coflnet.Sky.Commands.Shared;
 
@@ -43,51 +44,6 @@ public class ChestView
     [Key(1)]
     public string Name;
 }
-[MessagePackObject]
-public class Item
-{
-    /// <summary>
-    /// 
-    /// </summary>
-    [Key(0)]
-    public long? Id { get; set; }
-    /// <summary>
-    /// The item name for display
-    /// </summary>
-    [Key(1)]
-    public string ItemName { get; set; } = null!;
-    /// <summary>
-    /// Hypixel item tag for this item
-    /// </summary>
-    [Key(2)]
-    public string Tag { get; set; } = null!;
-    /// <summary>
-    /// Other aditional attributes
-    /// </summary>
-    [Key(3)]
-    public Dictionary<string, object>? ExtraAttributes { get; set; }
 
-    /// <summary>
-    /// Enchantments if any
-    /// </summary>
-    [Key(4)]
-    public Dictionary<string, byte>? Enchantments { get; set; }  = new();
-    /// <summary>
-    /// Color element
-    /// </summary>
-    [Key(5)]
-    public int? Color { get; set; } 
-    /// <summary>
-    /// Item Description aka Lore displayed in game, is a written form of <see cref="ExtraAttributes"/>
-    /// </summary>
-    [Key(6)]
-    public string? Description { get; set; }
-    /// <summary>
-    /// Stacksize
-    /// </summary>
-    [Key(7)]
-    public byte Count { get; set; }
-
-}
 
 #nullable restore
