b5425537f233d12d246e8b1434cb67cfacd6e6b4
Äkwav <ekwav@coflnet.com>
2023-09-18 19:53:44 +0200

    fix https://github.com/Coflnet/SkyFilter/issues/62

--- Helper/FlipFilters/ArmorSetDetailedFlipFilter.cs
+++ Helper/FlipFilters/ArmorSetDetailedFlipFilter.cs
@@ -9,14 +9,20 @@ using Coflnet.Sky.Filter;
 namespace Coflnet.Sky.Commands.Shared;
 public class ArmorSetDetailedFlipFilter : DetailedFlipFilter
 {
-    // xy_TERROR_LEGGINGS can't be sold on ah so they are excluded
-    public object[] Options => ItemDetails.Instance.TagLookup.Where(t => t.Key.EndsWith("_LEGGINGS") && !t.Key.Contains("_TERROR_")).Select(t => (object)t.Key.Replace("_LEGGINGS", "")).ToArray();
+    public object[] Options => ItemDetails.Instance.TagLookup
+        .Where(t => t.Key.EndsWith("_LEGGINGS") && IsOnAh(t))
+        .Select(t => (object)t.Key.Replace("_LEGGINGS", "")).ToArray();
+
+    private static bool IsOnAh(KeyValuePair<string, int> t)
+    {
+        return !t.Key.Contains("_TERROR_") && !t.Key.Contains("_CRIMSON_") && !t.Key.Contains("_AURORA_") && !t.Key.Contains("_FERVOR_") && !t.Key.Contains("_HOLLOW_");
+    }
 
     public FilterType FilterType => FilterType.Equal;
 
     public Expression<Func<FlipInstance, bool>> GetExpression(Dictionary<string, string> filters, string val)
     {
-        return flip => flip.Auction.Tag.StartsWith(val) 
+        return flip => flip.Auction.Tag.StartsWith(val)
             && (flip.Auction.Tag.EndsWith("_LEGGINGS") || flip.Auction.Tag.EndsWith("_CHESTPLATE") || flip.Auction.Tag.EndsWith("_HELMET") || flip.Auction.Tag.EndsWith("_BOOTS"));
     }
 }
