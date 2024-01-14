60dfe69ad832989c20d83d7927877a3bc65aeccf
Äkwav <ekwav@coflnet.com>
2024-01-14 12:57:30 +0100

    lock update

--- Services/FilterStateService.cs
+++ Services/FilterStateService.cs
@@ -3,6 +3,7 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Coflnet.Sky.Core;
 using Coflnet.Sky.Items.Client.Model;
@@ -23,6 +24,8 @@ public class FilterStateService
         public Dictionary<int, HashSet<string>> IntroductionAge { get; set; } = new();
         public HashSet<string> ExistingTags { get; set; } = new();
     }
+
+    private SemaphoreSlim updateLock = new SemaphoreSlim(1, 1);
 
     public FilterState State { get; set; } = new FilterState();
 
@@ -80,5 +83,46 @@ public class FilterStateService
             State.IntroductionAge[days] = new HashSet<string>(items);
         }
         return State.IntroductionAge[days];
+    }
+
+    public async Task UpdateState(FilterState newState)
+    {
+        if (updateLock.CurrentCount == 0)
+        {
+            return;
+        }
+        try
+        {
+            await updateLock.WaitAsync();
+            UpdateState(newState, State);
+        }
+        finally
+        {
+            updateLock.Release();
+        }
+    }
+
+    private static void UpdateState(FilterState newState, FilterState local)
+    {
+        local.CurrentMayor = newState.CurrentMayor;
+        local.NextMayor = newState.NextMayor;
+        local.PreviousMayor = newState.PreviousMayor;
+        foreach (var item in newState.ExistingTags)
+        {
+            local.ExistingTags.Add(item);
+        }
+        foreach (var day in newState.IntroductionAge)
+        {
+            local.IntroductionAge.TryAdd(day.Key, new());
+            foreach (var item in day.Value)
+            {
+                local.IntroductionAge[day.Key].Add(item);
+            }
+        }
+        foreach (var item in newState.itemCategories)
+        {
+            local.itemCategories.AddOrUpdate(item.Key, item.Value, (k, v) => v.Union(item.Value).ToHashSet());
+        }
+        newState.LastUpdate = DateTime.UtcNow;
     }
 }
