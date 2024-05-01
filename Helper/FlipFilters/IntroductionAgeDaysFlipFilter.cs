09680ddf37bae2521bdbcc06198561f4c0ec10db
Äkwav <ekwav@coflnet.com>
2024-05-01 23:09:46 +0200

    correct introduction age filter options

--- Helper/FlipFilters/IntroductionAgeDaysFlipFilter.cs
+++ Helper/FlipFilters/IntroductionAgeDaysFlipFilter.cs
@@ -4,16 +4,13 @@ using System.Collections.Generic;
 using System.Linq.Expressions;
 using Coflnet.Sky.Core;
 using Coflnet.Sky.Filter;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace Coflnet.Sky.Commands.Shared;
 public class IntroductionAgeDaysDetailedFlipFilter : DetailedFlipFilter
 {
-    public FilterType FilterType => FilterType.RANGE | FilterType.Equal;
+    public FilterType FilterType => FilterType.LOWER | FilterType.Equal;
 
-    public object[] Options => new object[] { 1, 20 };
-    //private HashSet<string> knownExisting;
-
+    public object[] Options => new object[] { 1, 14 };
 
     public Expression<Func<FlipInstance, bool>> GetExpression(Dictionary<string, string> filters, string val)
     {
