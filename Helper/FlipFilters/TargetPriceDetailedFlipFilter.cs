1761c0a552bd7a5cf7729a3ea1bdd4e46e2daa16
Äkwav <ekwav@coflnet.com>
2024-06-17 12:14:59 +0200

    correct Targetprice to use ranges

--- Helper/FlipFilters/TargetPriceDetailedFlipFilter.cs
+++ Helper/FlipFilters/TargetPriceDetailedFlipFilter.cs
@@ -8,8 +8,6 @@ namespace Coflnet.Sky.Commands.Shared;
 [FilterDescription("Filter for the price estimations")]
 public class TargetPriceDetailedFlipFilter : NumberDetailedFlipFilter
 {
-    public override FilterType FilterType => FilterType.NUMERICAL;
-
     protected override Expression<Func<FlipInstance, double>> GetSelector()
     {
         return (f) => f.Target;
