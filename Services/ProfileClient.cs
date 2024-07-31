11dab451036570ebb8421c3e0776723067267693
Äkwav <ekwav@coflnet.com>
2024-07-31 19:22:10 +0200

    extend profile client for crafts

--- Services/ProfileClient.cs
+++ Services/ProfileClient.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Coflnet.Sky.Crafts.Models;
 using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 using RestSharp;
 
 namespace Coflnet.Sky.Commands.Shared;
@@ -8,6 +10,9 @@ namespace Coflnet.Sky.Commands.Shared;
 public interface IProfileClient
 {
     Task<ProfileClient.ForgeData> GetForgeData(string playerId, string profile);
+    Task<List<ProfitableCraft>> FilterProfitableCrafts(Task<List<ProfitableCraft>> craftsTask, string playerId, string profileId);
+    Task<Dictionary<string, ProfileClient.CollectionElem>> GetCollectionData(string playerId, string profile);
+    Task<Dictionary<string, ProfileClient.SlayerElem>> GetSlayerData(string playerId, string profile);
 }
 
 public class ProfileClient : IProfileClient
@@ -24,11 +29,68 @@ public class ProfileClient : IProfileClient
         return response.Data;
     }
 
+    public async Task<Dictionary<string, CollectionElem>> GetCollectionData(string playerId, string profile)
+    {
+        var collectionJson = await profileClient.ExecuteAsync(new RestRequest($"/api/profile/{playerId}/{profile}/data/collections"));
+        var collection = JsonConvert.DeserializeObject<Dictionary<string, CollectionElem>>(collectionJson.Content);
+        return collection;
+    }
+
+    public async Task<Dictionary<string, SlayerElem>> GetSlayerData(string playerId, string profile)
+    {
+        var slayerJson = await profileClient.ExecuteAsync(new RestRequest($"/api/profile/{playerId}/{profile}/data/slayers"));
+        var slayer = JsonConvert.DeserializeObject<Dictionary<string, SlayerElem>>(slayerJson.Content);
+        return slayer;
+    }
+
+    public async Task<List<ProfitableCraft>> FilterProfitableCrafts(Task<List<ProfitableCraft>> craftsTask, string playerId, string profileId)
+    {
+        var collectionTask = GetCollectionData(playerId, profileId);
+        var slayers = await GetSlayerData(playerId, profileId);
+        var collection = await collectionTask;
+        var crafts = await craftsTask;
+        var list = new List<ProfitableCraft>();
+        foreach (var item in crafts)
+        {
+            if (item == null)
+                continue;
+
+            if (item.ReqCollection == null
+            || collection.TryGetValue(item.ReqCollection.Name, out CollectionElem elem)
+                    && elem.tier >= item.ReqCollection.Level)
+            {
+                list.Add(item);
+            }
+            else if (item.ReqSlayer == null
+                || slayers.TryGetValue(item.ReqSlayer.Name.ToLower(), out SlayerElem slayerElem)
+                  && slayerElem.Level.currentLevel >= item.ReqSlayer.Level)
+                list.Add(item);
+        }
+        return list;
+    }
+
     public class ForgeData
     {
         public int HotMLevel { get; set; }
         public float QuickForgeSpeed { get; set; }
         public Dictionary<string, int> CollectionLevels { get; set; }
+    }
+
+    public class SlayerElem
+    {
+        public SlayerLevel Level { get; set; }
+        public class SlayerLevel
+        {
+            public int currentLevel;
+        }
+    }
+
+    public class CollectionElem
+    {
+        /// <summary>
+        /// The collection tier/level this requires
+        /// </summary>
+        public int tier;
     }
 }
 
