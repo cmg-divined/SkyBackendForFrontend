0e4d088d7f16239d62803bb81db4a147635db97e
Äkwav <ekwav@coflnet.com>
2024-08-01 12:21:47 +0200

    remove time component for sessionId

--- Helper/IdConverter.cs
+++ Helper/IdConverter.cs
@@ -14,7 +14,7 @@ namespace Coflnet.Sky.Commands.Shared
         /// <returns></returns>
         public (long, string) ComputeConnectionId(string playerId, string sessionId)
         {
-            var bytes = Encoding.UTF8.GetBytes(playerId.ToLower() + sessionId + DateTime.Now.RoundDown(TimeSpan.FromDays(120)).ToString());
+            var bytes = Encoding.UTF8.GetBytes(playerId.ToLower() + sessionId + "fancySalt");
             var hash = System.Security.Cryptography.SHA512.Create();
             var hashed = hash.ComputeHash(bytes);
             return (BitConverter.ToInt64(hashed), Convert.ToBase64String(hashed, 0, 16));
