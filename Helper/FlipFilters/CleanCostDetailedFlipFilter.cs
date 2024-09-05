f22484d5c09feacd199ebca0a9beb18edc70c5bd
Äkwav <ekwav@coflnet.com>
2024-09-05 12:57:31 +0200

    remove type override from clean cost filter

--- Helper/FlipFilters/CleanCostDetailedFlipFilter.cs
+++ Helper/FlipFilters/CleanCostDetailedFlipFilter.cs
@@ -8,8 +8,6 @@ namespace Coflnet.Sky.Commands.Shared;
 [FilterDescription("How much the item usually sells for without any modifiers")]
 public class CleanCostDetailedFlipFilter : NumberDetailedFlipFilter
 {
-    public override FilterType FilterType => FilterType.NUMERICAL;
-
     protected override Expression<Func<FlipInstance, double>> GetSelector(FilterContext filters)
     {
         return f => f.Context.ContainsKey("cleanCost") ? double.Parse(f.Context["cleanCost"]) : 0;
