23c22abeec46d6da50931cb10ed13c9396579997
Äkwav <ekwav@coflnet.com>
2024-09-21 15:52:50 +0200

    allow -1 in listing slots left

--- Helper/FlipFilters/ListingSlotsLeft.cs
+++ Helper/FlipFilters/ListingSlotsLeft.cs
@@ -8,7 +8,7 @@ namespace Coflnet.Sky.Commands.Shared;
 [FilterDescription("How many ah spaces are left, is -1 if not supported and 0 if not known")]
 public class ListingSlotsLeft : NumberDetailedFlipFilter
 {
-    public override object[] Options => new object[] { 1, 26 };
+    public override object[] Options => new object[] { -1, 26 };
     protected override Expression<Func<FlipInstance, double>> GetSelector(FilterContext filters)
     {
         return f => filters.playerInfo == null ? -1 : filters.playerInfo.AhSlotsOpen;
