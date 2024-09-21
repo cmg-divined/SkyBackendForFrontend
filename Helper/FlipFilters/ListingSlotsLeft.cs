95ec124103a770868227e6f8bbbc8c7bfe31606d
Äkwav <ekwav@coflnet.com>
2024-09-21 14:56:39 +0200

    correct listing slots left expression

--- Helper/FlipFilters/ListingSlotsLeft.cs
+++ Helper/FlipFilters/ListingSlotsLeft.cs
@@ -8,10 +8,9 @@ namespace Coflnet.Sky.Commands.Shared;
 [FilterDescription("How many ah spaces are left, is -1 if not supported and 0 if not known")]
 public class ListingSlotsLeft : NumberDetailedFlipFilter
 {
-    public override object[] Options => new object[] { 1, 20 };
+    public override object[] Options => new object[] { 1, 26 };
     protected override Expression<Func<FlipInstance, double>> GetSelector(FilterContext filters)
     {
-        var slots = filters.playerInfo?.AhSlotsOpen ?? -1;
-        return f => slots;
+        return f => filters.playerInfo == null ? -1 : filters.playerInfo.AhSlotsOpen;
     }
 }
